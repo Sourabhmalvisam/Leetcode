2ee404765e9ce57b18413e61409fac2b1e41b104
Sourabhmalvisam <78964331+Sourabhmalvisam@users.noreply.github.com>
2023-09-01 14:00:32 +0530

    Time: 1 ms (99.42%) | Memory: 43.6 MB (64.79%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,18 @@
+class Solution { //Blind75 //Striver //LeetcodePatterns //Arrays & Hashing
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashmap = new HashMap<>(); // HashMap to store numbers and their indices.
+        
+        for (int i = 0; i < nums.length; i++) {
+            int val = nums[i];
+            int diff = target - val; // Calculate the difference
+            
+            if (hashmap.containsKey(diff)) {
+                return new int[] { hashmap.get(diff), i }; // Return indices if difference is found
+            }
+            
+            hashmap.put(val, i); // Add current value to the HashMap
+        }
+        
+        return new int[0]; // Return empty array if no solution is found
+    }
+}
