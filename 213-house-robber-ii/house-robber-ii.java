255f26011536ae23164ba387fca6eeebe7b7b52f
Sourabhmalvisam <78964331+Sourabhmalvisam@users.noreply.github.com>
2023-09-15 14:37:46 +0530

    Time: 0 ms (100.00%) | Memory: 39.6 MB (90.26%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,20 +1,27 @@
 class Solution {
-    public int rob(int[] nums) {
-        int n=nums.length;
-        if(n==1) return nums[0];
-        return Math.max(solve(Arrays.copyOfRange(nums, 1, n)),solve(Arrays.copyOfRange(nums,0,n-1)));
+    public int rob(int[] arr) {
+        
+    int n = arr.length;
+    if(n==1) return arr[0];
+        int ans1 = houseRobbarBottomUp(Arrays.copyOfRange(arr, 1, n));
+        int ans2 = houseRobbarBottomUp(Arrays.copyOfRange(arr, 0, n - 1));
+
+        return Math.max(ans1, ans2);
     }
-    public static int solve(int arr[]){
-        int prev=arr[0];
-        int prev2=0;
-        for(int i=1;i<arr.length;i++){
-            int pick=arr[i];
-            if(i>1) pick+=prev2;
-            int notPick=prev;
-            int cur_i=Math.max(pick,notPick);
-            prev2=prev;
-            prev=cur_i;
+  static int houseRobbarBottomUp(int[] arr) {
+        int n = arr.length;
+        int dp[] = new int[n];
+        Arrays.fill(dp, -1);
+        dp[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            int pick = arr[i];
+            if (i > 1)
+                pick += dp[i - 2];
+            int notPick = dp[i - 1];
+            dp[i] = Math.max(pick, notPick);
         }
-        return prev;
+
+        return dp[n - 1];
     }
 }
