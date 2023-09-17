e9e6edb3ebc435d3347fcbf424403949d88f04cb
Sourabhmalvisam <78964331+Sourabhmalvisam@users.noreply.github.com>
2023-09-18 00:13:26 +0530

    Time: 110 ms (8.16%) | Memory: 43.7 MB (34.88%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,25 +1,26 @@
 class Solution {
-    static int[][] dp;
-    public static int solve(int[] coins,int idx, int total){
-        if(idx==0){
-            if(total%coins[0]==0) return total/coins[0];
-            return (int)Math.pow(10,9);
-        }
-        if(dp[idx][total]!=-1) return dp[idx][total];
-        int notTake=solve(coins, idx-1,total);
-        int take=(int)Math.pow(10,9);
-        if(coins[idx]<=total){
-            take=1+solve(coins, idx,total-coins[idx]);
-        }
-        return dp[idx][total]=Math.min(take,notTake);
-    }
+   
     public int coinChange(int[] coins, int amount) {
-        dp = new int[coins.length][amount+1];
-        for(int [] row: dp){
-            Arrays.fill(row,-1);
+     int[][]   dp = new int[coins.length][amount+1];
+        for(int j=0;j<=amount;j++){
+            if(j%coins[0]==0) dp[0][j]=j/coins[0];
+            else dp[0][j]=(int )Math.pow(10,9);
         }
-        int ans= solve(coins, coins.length-1,amount);
-        if(ans!=(int)Math.pow(10,9)) return ans;
-        else return -1;
+        for(int i=1;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+
+        int notTake=dp[i-1][j];
+        int take=(int)Math.pow(10,9);
+      if (coins[i] <= amount && j - coins[i] >= 0) {
+    take = 1 + dp[i][j-coins[i]]; // Use dp[i-1] here
+}
+
+        dp[i][j]=Math.min(take,notTake);
+            }
+        }
+        int ans=dp[coins.length-1][amount];
+        if(ans>=(int)Math.pow(10,9)) return -1;
+        else return ans;
+        
     }
 }
