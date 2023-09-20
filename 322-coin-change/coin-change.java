d4690d0223c03584ea1f074050796e831b104163
Sourabhmalvisam <78964331+Sourabhmalvisam@users.noreply.github.com>
2023-09-20 16:26:37 +0530

    Time: 65 ms (11.49%) | Memory: 43.9 MB (24.46%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,24 +1,36 @@
 class Solution {
-    public static int solve(int arr[], int idx, int amount,int[][]dp){
-        if(idx==0){
-            if(amount%arr[idx]==0) return amount/arr[idx];
-            else return (int)Math.pow(10,9);
+    public static int coinChange(int[] coins, int idx, int amount, int[][] dp) {
+        if (idx == 0) {
+            if (amount % coins[idx] == 0) {
+                return amount / coins[idx];
+            } else {
+                return Integer.MAX_VALUE;
+            }
         }
-        if(dp[idx][amount]!=-1) return dp[idx][amount];
-        int notTake=solve(arr,idx-1,amount,dp);
-        int take=(int)Math.pow(10,9);
-        if(arr[idx]<=amount) take=1+solve(arr,idx,amount-arr[idx],dp);
-        int ans=Math.min(notTake, take);
-        return dp[idx][amount]=ans;
+        if (dp[idx][amount] != -1) {
+            return dp[idx][amount];
+        }
+        int notTake = coinChange(coins, idx - 1, amount, dp);
+        int take = (int)Math.pow(10,9);
+        if (coins[idx] <= amount) {
+            take = 1 + coinChange(coins, idx, amount - coins[idx], dp);
+        }
+        int ans = Math.min(notTake, take);
+        dp[idx][amount] = ans;
+        return ans;
     }
+
     public int coinChange(int[] coins, int amount) {
-        int dp[][] = new int[coins.length][amount+1];
-        for(int [] row: dp){
-            Arrays.fill(row,-1);
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
-        int ans= solve(coins, coins.length-1,amount,dp);
-        if(ans>=(int)Math.pow(10,9))
-        return -1; else return ans;
-    
+        int ans = coinChange(coins, n - 1, amount, dp);
+        if (ans >= (int)Math.pow(10,9)) {
+            return -1;
+        } else {
+            return ans;
+        }
     }
 }
