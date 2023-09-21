9ce549b125b75dd2e23d00ca0fc2e9760bf61e5a
Sourabhmalvisam <78964331+Sourabhmalvisam@users.noreply.github.com>
2023-09-21 17:10:29 +0530

    Time: 25 ms (44.85%) | Memory: 44 MB (80.42%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,29 +1,46 @@
-public class Solution {
-    public boolean isMatch(String s, String p) {
-        int m = s.length();
-        int n = p.length();
-        boolean[][] dp = new boolean[m + 1][n + 1];
+class Solution { //Striver //DP
+	public boolean isMatch(String S2, String S1) {
+		int m = S1.length();
+		int n = S2.length();
 
-        // Initialize dp matrix
-        dp[0][0] = true;
-        for (int i = 1; i <= n; i++) {
-            if (p.charAt(i - 1) == '*') {
-                dp[0][i] = dp[0][i - 1];
-            }
-        }
+		boolean dp[][] = new boolean[m + 1][n + 1];
+		dp[0][0] = true;
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else if (p.charAt(j - 1) == '*') {
-                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
-                } else {
-                    dp[i][j] = false;
-                }
-            }
-        }
+		for (int j = 1; j <= n; j++) {
+			dp[0][j] = false;
+		}
 
-        return dp[m][n];
-    }
+		for (int i = 1; i <= m; i++) {
+			dp[i][0] = isAllStars(S1, i);
+		}
+
+		for (int i = 1; i <= m; i++) {
+			for (int j = 1; j <= n; j++) {
+
+				if (S1.charAt(i - 1) == S2.charAt(j - 1) || S1.charAt(i - 1) == '?')
+					dp[i][j] = dp[i - 1][j - 1];
+
+				else {
+					if (S1.charAt(i - 1) == '*')
+						dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+
+					else
+						dp[i][j] = false;
+				}
+			}
+		}
+
+		return dp[m][n];
+	}
+
+	private boolean isAllStars(String str, int end) {
+		for (int i = 0; i < end; i++) {
+			if (str.charAt(i) != '*') {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
 }
