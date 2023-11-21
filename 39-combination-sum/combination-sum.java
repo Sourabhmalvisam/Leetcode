759f1d6d8b1a698425598c0ebcc28297438b4bc4
Sourabhmalvisam <78964331+Sourabhmalvisam@users.noreply.github.com>
2023-11-22 03:26:33 +0530

    Time: 2 ms (75.79%) | Memory: 44.3 MB (7.13%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,22 +1,19 @@
 class Solution {
 
-	public List < List<Integer>> combinationSum(int[] candidates, int target) {
-		List < List<Integer>> ans = new ArrayList<>();
-		findCombinations2(0, candidates, target, ans, new ArrayList<>());
-		return ans;
+	public List < List<Integer>> combinationSum(int[] nums, int target) {
+		List < List<Integer>> list = new ArrayList<>();
+		backtrack(list, new ArrayList<>(), nums, target, 0);
+		return list;
 	}
 
-	static void findCombinations2(int ind, int[] arr, int target, List < List<Integer>> ans, List<Integer> ds) {
-		if (target == 0) {
-			ans.add(new ArrayList<>(ds));
-			return;
-		}
-
-		for (int i = ind; i < arr.length; i++) {
-			if (arr[i] <= target) {
-				ds.add(arr[i]);
-				findCombinations2(i, arr, target - arr[i], ans, ds);
-				ds.remove(ds.size() - 1);
+	private void backtrack(List < List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start) {
+		if (remain < 0) return;
+		else if (remain == 0) list.add(new ArrayList<>(tempList));
+		else {
+			for (int i = start; i < nums.length; i++) {
+				tempList.add(nums[i]);
+				backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
+				tempList.remove(tempList.size() - 1);
 			}
 		}
 	}
