dc75d5afd57ceaa0089bd8eeb3825775ae148b0c
Sourabhmalvisam <78964331+Sourabhmalvisam@users.noreply.github.com>
2024-01-15 18:41:55 +0530

    Time: 89 ms (45.26%) | Memory: 103.6 MB (31.28%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
@@ -2,26 +2,25 @@ class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
       Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<matches.length;i++){
-            int winner=matches[i][0];
-            map.put(winner,map.getOrDefault(winner,0));
             int loser=matches[i][1];
             int timesLost=map.getOrDefault(loser,0);
             map.put(loser,timesLost+1);
             
 
         }
-        List<Integer> winner=new ArrayList<>();
+        List<Integer> winners=new ArrayList<>();
         List<Integer> losers=new ArrayList<>();
-        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
-            int key=entry.getKey();
-            int value=entry.getValue();
-            if(value==0) winner.add(key);
-            if(value==1) losers.add(key);
-        }
-        Collections.sort(winner);
+       for(int i=0;i<matches.length;i++){
+           int winner=matches[i][0];
+           int loser=matches[i][1];
+           if(!map.containsKey(winner)) {winners.add(winner);
+           map.put(winner,0);}
+           if(map.get(loser)==1) losers.add(loser);
+       }
+        Collections.sort(winners);
         Collections.sort(losers);
         List<List<Integer>> ans=new ArrayList<>();
-        ans.add(winner);
+        ans.add(winners);
         ans.add(losers);
         return ans;
     }
