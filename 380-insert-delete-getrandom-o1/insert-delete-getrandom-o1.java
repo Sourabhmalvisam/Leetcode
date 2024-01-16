45ad204f7ca15972891cad6641e69b0fdf1d92ea
Sourabhmalvisam <78964331+Sourabhmalvisam@users.noreply.github.com>
2024-01-16 19:12:08 +0530

    Time: 60 ms (37.36%) | Memory: 89.2 MB (84.37%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
@@ -1,37 +1,33 @@
 class RandomizedSet {
     Set<Integer> set;
+    List<Integer> list;
 
     public RandomizedSet() {
         set=new HashSet<>();
+        list=new ArrayList<>();
         
     }
     
     public boolean insert(int val) {
         if(!set.contains(val))
-        {set.add(val);
-        return true;}
+        {set.add(val);list.add(val);
+        return true; }
         return false;
         
     }
     
     public boolean remove(int val) {
         if(set.contains(val))
-        {set.remove(val); return true;}
+        {set.remove(val); list.remove(Integer.valueOf(val)); return true;}
         return false;
     }
     
     public int getRandom() {
-        int size = set.size();
-int item = new Random().nextInt(size); // In real life, the Random object should be rather more shared than this
-int i = 0;
-for(int obj : set)
-{
-    if (i == item)
-        return obj;
-    i++;
-}
-return 0;
+       int random=new Random().nextInt(list.size());
+     return   list.get(random);
+        
     }
+       
 }
 
 /**
