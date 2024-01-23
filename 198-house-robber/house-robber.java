636a0045cf7c7ace1d05c97a843666f26de61d44
Sourabhmalvisam <78964331+Sourabhmalvisam@users.noreply.github.com>
2024-01-23 23:40:16 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (41.40%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,15 +1,30 @@
 class Solution {
-    
     public int rob(int[] nums) {
-      int[]  dp=new int[nums.length];
-        Arrays.fill(dp,-1);
-      dp[0]=nums[0];
-      for(int i=1;i<nums.length;i++){
-          int pick=nums[i];
-          if(i-2>=0) pick+=dp[i-2];
-          int notPick=dp[i-1];
-          dp[i]=Math.max(pick,notPick);
-      }
-      return dp[nums.length-1];
+        int [] dp=new int[nums.length];
+      return  solveUtil(nums.length,nums,dp);
     }
+      static int solveUtil(int n, int[] arr, int[] dp) {
+        // Initialize the dp array with the first element of the input array.
+        dp[0] = arr[0];
+
+        // Iterate through the input array to fill the dp array.
+        for (int i = 1; i < n; i++) {
+            // Calculate the maximum sum by either picking the current element or not picking it.
+            int pick = arr[i];
+            
+            // If there are at least two elements before the current element, add the value from dp[i-2].
+            if (i > 1)
+                pick += dp[i - 2];
+            
+            // The non-pick option is to use the maximum sum from the previous element.
+            int nonPick = dp[i - 1];
+
+            // Store the maximum of the two options in the dp array for the current index.
+            dp[i] = Math.max(pick, nonPick);
+        }
+
+        // The final element of the dp array contains the maximum possible sum.
+        return dp[n - 1];
+    }
+
 }
