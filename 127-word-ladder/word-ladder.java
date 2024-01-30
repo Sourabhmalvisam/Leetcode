20328f0ac104853621681ff9620e9c631aa209b7
Sourabhmalvisam <78964331+Sourabhmalvisam@users.noreply.github.com>
2024-01-30 20:14:33 +0530

    Time: 79 ms (68.12%) | Memory: 46.6 MB (52.48%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,57 +1,41 @@
 class Solution {
-    public int ladderLength(String startWord, String targetWord, List<String> wordList) {
-        // Creating a queue ds of type {word, transitions to reach ‘word’}.
-        Queue<Pair> q = new LinkedList<>();
-
-        // BFS traversal with pushing values in queue
-        // when after a transformation, a word is found in wordList.
-        q.add(new Pair(startWord, 1));
-
-        // Push all values of wordList into a set
-        // to make deletion from it easier and in less time complexity.
-        Set<String> st = new HashSet<>();
-        st.addAll(wordList);
-
-        st.remove(startWord);
-        while (!q.isEmpty()) {
-            String word = q.peek().first;
-            int steps = q.peek().second;
-            q.remove();
-
-            // we return the steps as soon as
-            // the first occurrence of targetWord is found.
-            if (word.equals(targetWord)) {
-                return steps;
-            }
-
-            // Now, replace each character of ‘word’ with char
-            // from a-z then check if ‘word’ exists in wordList.
-            for (int i = 0; i < word.length(); i++) {
-                for (char ch = 'a'; ch <= 'z'; ch++) {
-                    char[] replacedCharArray = word.toCharArray();
-                    replacedCharArray[i] = ch;
-                    String replacedWord = new String(replacedCharArray);
-
-                    // check if it exists in the set and push it in the queue.
-                    if (st.contains(replacedWord)) {
-                        st.remove(replacedWord);
-                        q.add(new Pair(replacedWord, steps + 1));
-                    }
-                }
-            }
+    class Pair{
+        String word;
+        int step;
+        Pair(String w, int s){
+            word=w;
+            step=s;
         }
-        // If there is no transformation sequence possible
+    }
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Queue<Pair> q=new LinkedList<>();
+        Set<String> set=new HashSet<>();
+        for(String s: wordList){
+            set.add(s);
+        }
+        set.remove(beginWord);
+        q.add(new Pair(beginWord,1));
+
+        while(!q.isEmpty()){
+            Pair curr=q.poll();
+            String word=curr.word;
+            int steps=curr.step;
+            if(word.equals(endWord)) return steps;
+           
+           for(int i=0;i<word.length();i++){
+                char[] wordArr=word.toCharArray();
+              for(char ch='a';ch<='z';ch++){
+                  wordArr[i]=ch;
+                  String newWord=new String(wordArr);
+                  if(set.contains(newWord)){
+                      set.remove(newWord);
+                      q.add(new Pair(newWord,steps+1));
+                  }
+              }
+               
+           }
+        }
         return 0;
+        
     }
 }
-class Pair {
-    String first;
-    int second;
-
-    Pair(String first, int second) {
-        this.first = first;
-        this.second = second;
-    }
-}
-
-
