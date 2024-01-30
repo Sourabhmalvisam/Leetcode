814f52008825fe535ab11b58cb59c2c717c34d64
Sourabhmalvisam <78964331+Sourabhmalvisam@users.noreply.github.com>
2024-01-30 19:36:02 +0530

    Time: 2 ms (79.26%) | Memory: 42 MB (67.60%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,55 +1,68 @@
 class Solution {
-    class Pair{
+    class Orange {
         int row;
         int col;
         int time;
-        Pair(int r, int c, int t){
-            row=r;
-            col=c;
-            time=t;
-        }
 
+        Orange(int r, int c, int t) {
+            row = r;
+            col = c;
+            time = t;
+        }
     }
+
     public int orangesRotting(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        int [][]vis=new int[m][n];
-        Queue<Pair> q=new LinkedList<>();
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int[][] visited = new int[rows][cols];
+        Queue<Orange> queue = new LinkedList<>();
 
-        int countFresh=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
-                    countFresh++;
-                   
-                }
-                else if(grid[i][j]==2){
-                    q.add(new Pair(i,j,0));
-                    vis[i][j]=2;
+        int freshOrangesCount = 0;
+
+        // Initialize the queue with rotten oranges and count fresh oranges
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    freshOrangesCount++;
+                } else if (grid[i][j] == 2) {
+                    queue.add(new Orange(i, j, 0));
+                    visited[i][j] = 2;
                 }
             }
         }
-        int maxTime=0;
-        int [][]dir={{0,1},{0,-1},{1,0},{-1,0}};
-        int count=0;
-        while(!q.isEmpty()){
-            Pair curr=q.poll();
-            int row=curr.row;
-            int col=curr.col;
-            int time=curr.time;
-            maxTime=Math.max(time, maxTime);
-            for(int [] d: dir){
-                int newR=row+d[0];
-                int newC=col+d[1];
-                if(newR>=0&newC>=0&&newR<m&&newC<n&&vis[newR][newC]==0&&grid[newR][newC]==1){
-                    vis[newR][newC]=2;
-                    q.add(new Pair(newR,newC,time+1));
-                    count++;
 
+        int maxTime = 0;
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        int rottenOrangesCount = 0;
+
+        while (!queue.isEmpty()) {
+            Orange currentOrange = queue.poll();
+            int currentRow = currentOrange.row;
+            int currentCol = currentOrange.col;
+            int currentTime = currentOrange.time;
+            maxTime = Math.max(currentTime, maxTime);
+
+            // Explore neighboring oranges
+            for (int[] dir : directions) {
+                int newRow = currentRow + dir[0];
+                int newCol = currentCol + dir[1];
+
+                // Check if the neighbor is within bounds and is a fresh orange
+                if (newRow >= 0 && newCol >= 0 && newRow < rows && newCol < cols &&
+                    visited[newRow][newCol] == 0 && grid[newRow][newCol] == 1) {
+                    visited[newRow][newCol] = 2;
+                    queue.add(new Orange(newRow, newCol, currentTime + 1));
+                    rottenOrangesCount++;
                 }
             }
         }
-        if(count==countFresh) return maxTime;
+
+        // Check if all fresh oranges were reached
+        if (rottenOrangesCount == freshOrangesCount) {
+            return maxTime;
+        }
+
+        // Some fresh oranges couldn't be reached
         return -1;
     }
 }
