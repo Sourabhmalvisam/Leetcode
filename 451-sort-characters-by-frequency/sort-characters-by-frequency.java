6d2c2ab771fb6ac6e811a8c328e6063538481593
Sourabhmalvisam <78964331+Sourabhmalvisam@users.noreply.github.com>
2024-02-08 19:52:40 +0530

    Time: 382 ms (15.90%) | Memory: 45.7 MB (40.70%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,40 +1,38 @@
-import java.util.*;
-
 class Solution {
-    public class Pair implements Comparable<Pair> {
+    class Pair implements Comparable<Pair> {
         char ch;
         int freq;
-
-        Pair(char c, int f) {
-            ch = c;
-            freq = f;
+        Pair (char c, int f){
+            ch=c;
+            freq=f;
         }
-
         @Override
-        public int compareTo(Pair other) {
-            return Integer.compare(other.freq, this.freq);
+        public int compareTo(Pair o){
+            return o.freq-this.freq;
         }
     }
-
     public String frequencySort(String s) {
-        Map<Character, Integer> charFrequency = new HashMap<>();
-        for (char ch : s.toCharArray()) {
-            charFrequency.put(ch, charFrequency.getOrDefault(ch, 0) + 1);
+        Map<Character,Integer> map=new HashMap<>();
+        char [] charArr=s.toCharArray();
+        for(char ch: charArr){
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
-
-        List<Pair> pairs = new ArrayList<>();
-        for (Map.Entry<Character, Integer> entry : charFrequency.entrySet()) {
-            pairs.add(new Pair(entry.getKey(), entry.getValue()));
+        PriorityQueue<Pair> pq=new PriorityQueue<>();
+        for(Map.Entry<Character,Integer> entry: map.entrySet()){
+            pq.add(new Pair(entry.getKey(),entry.getValue()));
         }
-
-        Collections.sort(pairs);
-
-        StringBuilder ans = new StringBuilder();
-        for (Pair pair : pairs) {
-            for (int i = 0; i < pair.freq; i++) {
-                ans.append(pair.ch);
+        String ans="";
+        while(!pq.isEmpty()){
+            Pair p=pq.poll();
+            char ch=p.ch;
+            int freq=p.freq;
+            while(freq>0){
+                ans+=ch;
+                freq--;
             }
+
         }
-        return ans.toString();
+        return ans;
+
     }
 }
