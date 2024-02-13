3efd5994c3a1bc20ac7cd695c7ecb21cbee5c84f
Sourabhmalvisam <78964331+Sourabhmalvisam@users.noreply.github.com>
2024-02-13 22:54:38 +0530

    Time: 26 ms (42.97%) | Memory: 44.9 MB (74.36%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,31 +1,32 @@
 class Solution {
-    public int timeToEat(int []piles, int rate){
-        int hours=0;
-        for(int i=0;i<piles.length;i++){
-            hours+=Math.ceil(piles[i]/(double)rate);
-        }
-        return hours;
-    }
-
     public int minEatingSpeed(int[] piles, int h) {
-        int max=(int)-10e8;
-        for(int i=0;i<piles.length;i++){
-            max=Math.max(max,piles[i]);
-        }
-
         int low=1;
-        int high=max;
+        int high=findMax(piles);
         int ans=0;
         while(low<=high){
             int mid=low+(high-low)/2;
-
-            int timeToEat=timeToEat(piles,mid);
-            if(timeToEat<=h){
-                ans=mid;
-                high=mid-1;
-            }
-            else low=mid+1;
+            if(isPossible(piles,mid,h)) 
+                {
+                    ans=mid;
+                    high=mid-1;
+                }
+         else low=mid+1;
         }
         return ans;
+    }
+    public static boolean isPossible(int [] piles, int hourlyRate,int h){
+        int timeTaken=0;
+        for(int i=0;i<piles.length;i++){
+            timeTaken+=Math.ceil(piles[i]/(double)hourlyRate);
+        }
+        if(timeTaken<=h) return true;
+        return false;
+    }
+    public static int findMax(int [] piles){
+        int max=Integer.MIN_VALUE;
+        for(int x: piles){
+            max=Math.max(x,max);
+        }
+        return max;
     }
 }
